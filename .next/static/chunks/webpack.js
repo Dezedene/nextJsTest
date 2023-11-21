d6fe8ab2603e8d14e7aeddceeb7bca2927815cc9
Dezedene <gregoire.suray@gmail.com>
2023-11-21 13:53:03 +0100

    test live updates on server

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6bd18bd0bc07c2f7"; }
+/******/ 		__webpack_require__.h = function() { return "ff682d05aa5e96ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
